87581d374f664d20b28e36fb74c8564246125df6
Felipe O. Gonçalves <flpdeoliveiragcv@gmail.com>
2022-07-22 19:34:39 -0300

    Include Ipeadata wrapper

---
+++ R/wrap_ipea.R
@@ -0,0 +1,19 @@
+#' From Ipeadata Series To Tidy Dataframes
+#'
+#' @export
+wrap_ipea <- function(code, colname = "series") {
+
+  colname <- rlang::sym(colname)
+  api <- "http://ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='%s')"
+
+  series_req <- httr2::request(sprintf(api, code))
+  series_resp <- httr2::req_perform(series_req)
+
+  series_lst <- httr2::resp_body_json(series_resp, simplifyVector = TRUE)
+  series_df <- series_lst$value
+
+  series_df |>
+    tibble::as_tibble() |>
+    dplyr::select(dt = VALDATA, !!colname := VALVALOR) |>
+    dplyr::mutate(dt = as.Date(dt))
+}
