d3de0eb70a4cc2f7cb0ec5ae1e5d306b69e6a2f8
Felipe O. Gonçalves <flpdeoliveiragcv@gmail.com>
2022-07-22 19:39:14 -0300

    Adapt to new package objects

--- R/deflate.R
+++ R/deflate.R
@@ -6,7 +6,7 @@ deflate <- function(values, dates, ref_date) {
   code <- "PRECOS12_IPCA12"
 
   code |>
-    ipeadata() |>
+    wrap_ipea() |>
     dplyr::mutate(idx = series[dt == ref_date] / series) |>
     dplyr::summarize(idx[match(dates, dt)] * values) |>
     dplyr::pull()
